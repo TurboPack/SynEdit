8250743b6e6b318f3e60979517323434c27f357a
pyscripter <pyscripter@gmail.com>
2020-09-08 04:54:58 +0300

    Utf8 encode SynEditHighlighter.pas and SynEditSearch.pas to avoid messing up non-ascii characters.

--- Source/SynEditHighlighter.pas
+++ Source/SynEditHighlighter.pas
@@ -1,4 +1,4 @@
-{-------------------------------------------------------------------------------
+ï»¿{-------------------------------------------------------------------------------
 The contents of this file are subject to the Mozilla Public License
 Version 1.1 (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
@@ -12,7 +12,7 @@ The Original Code is: SynEditHighlighter.pas, released 2000-04-07.
 The Original Code is based on mwHighlighter.pas by Martin Waldenburg, part of
 the mwEdit component suite.
 Portions created by Martin Waldenburg are Copyright (C) 1998 Martin Waldenburg.
-Unicode translation by Maël Hörz.
+Unicode translation by MaÃ«l HÃ¶rz.
 Options property added by CodehunterWorks
 All Rights Reserved.
 
@@ -1103,8 +1103,8 @@ end;
 function TSynCustomHighlighter.IsWordBreakChar(AChar: WideChar): Boolean;
 begin
   case AChar of
-    '.', ',', ';', ':', '"', '''', '´', '`', '°', '^', '!', '?', '&',
-    '$', '@', '§', '%', '#', '~', '[', ']', '(', ')', '{', '}', '<', '>',
+    '.', ',', ';', ':', '"', '''', 'Â´', '`', 'Â°', '^', '!', '?', '&',
+    '$', '@', 'Â§', '%', '#', '~', '[', ']', '(', ')', '{', '}', '<', '>',
     '-', '=', '+', '*', '/', '\', '|':
       Result := True;
     else
--- Source/SynEditSearch.pas
+++ Source/SynEditSearch.pas
@@ -1,4 +1,4 @@
-{-------------------------------------------------------------------------------
+ï»¿{-------------------------------------------------------------------------------
 The contents of this file are subject to the Mozilla Public License
 Version 1.1 (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
@@ -13,7 +13,7 @@ The Original Code is: SynEditSearch.pas, released 2000-04-07.
 The Original Code is based on the mwEditSearch.pas file from the mwEdit
 component suite by Martin Waldenburg and other developers.
 Portions created by Martin Waldenburg are Copyright 1999 Martin Waldenburg.
-Unicode translation by Maël Hörz.
+Unicode translation by MaÃ«l HÃ¶rz.
 All Rights Reserved.
 
 Contributors to the SynEdit project are listed in the Contributors.txt file.
@@ -157,8 +157,8 @@ end;
 function IsWordBreakChar(C: WideChar): Boolean;
 begin
   case C of
-    #0..#32, '.', ',', ';', ':', '"', '''', '´', '`', '°', '^', '!', '?', '&',
-    '$', '@', '§', '%', '#', '~', '[', ']', '(', ')', '{', '}', '<', '>',
+    #0..#32, '.', ',', ';', ':', '"', '''', 'Â´', '`', 'Â°', '^', '!', '?', '&',
+    '$', '@', 'Â§', '%', '#', '~', '[', ']', '(', ')', '{', '}', '<', '>',
     '-', '=', '+', '*', '/', '\', '|':
       Result := True;
     else
