b31e07b6fb37642c14e297cf2242963f0fedee23
pyscripter <pyscripter@gmail.com>
2024-05-11 12:22:42 +0100

    Fix #80

--- Source/SynEditUndo.pas
+++ Source/SynEditUndo.pas
@@ -391,7 +391,7 @@ begin
     Dec(FBlockCount);
     if FBlockCount = 0 then
     begin
-      if FUndoList.Peek = FBlockSelRestoreItem then
+      if not FUndoList.IsEmpty and (FUndoList.Peek = FBlockSelRestoreItem) then
         // No undo items added from BlockBegin to BlockEnd
         FUndoList.Pop
       else
